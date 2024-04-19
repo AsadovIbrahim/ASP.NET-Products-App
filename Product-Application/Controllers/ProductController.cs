6b237b4cb330e0d7450581ee699e4a45230ebd67
AsadovIbrahim <ibrahimasadov31@gmail.com>
2024-04-20 02:25:54 +0400

    commit

---
+++ Product-Application/Controllers/ProductController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Product_Application.Models;
+
+namespace Product_Application.Controllers
+{
+    public class ProductController : Controller
+    {
+        public List<Product> Products { get; set; } = new List<Product>();
+
+        public ProductController()
+        {
+            Products.Add(new Product(1,"Snickers", 2.99, "Candy"));
+            Products.Add(new Product(2,"Cola", 0.85, "Drink"));
+            Products.Add(new Product(3,"Sprite", 0.82, "Drink"));
+            Products.Add(new Product(4,"Chocolate", 2, "Candy"));
+        }
+
+        public IActionResult GetAllProducts()
+        {
+            return View(Products);
+        }
+        public IActionResult GetProductById(int id)
+        {
+            foreach (var item in Products) {
+            
+                if(item.Id == id) return View(item);
+            }
+            return View("GetAllProducts", Products);
+        }
+
+        public IActionResult RemoveProduct(int id)
+        {
+            var selectedProduct = Products.FirstOrDefault(p => p.Id==id);
+            if (selectedProduct != null)
+            {
+                Products.Remove(selectedProduct);
+            }
+            return View("GetAllProducts", Products);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Product-Application/Models/Product.cs
@@ -0,0 +1,27 @@
+﻿namespace Product_Application.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ?Name { get; set; }
+        public double Price { get; set; }
+        public string? Description { get; set; }
+
+        //public Product()
+        //{
+        //    Id = ++_Id;
+        //}
+        public Product(int id,string name,double price,string description)
+        {
+            //Id= ++_Id;
+            Id= id;
+            Name = name;
+            Price = price;
+            Description = description;
+        }
+        public override string ToString()
+        {
+            return $"Id:{Id}\nName:{Name}\nPrice:{Price}\nDescription:{Description}";
+        }
+    }
+}
